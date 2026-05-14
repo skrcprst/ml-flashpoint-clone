22b0f5a55b2afb6687a577ca2676c75e186a8095
skrcprst <59887908+skrcprst@users.noreply.github.com>
2026-05-14 15:16:32 +1000

    Override dest PR number

--- tests/test_foo.py
+++ tests/test_foo.py
@@ -1,7 +1,30 @@
+from datetime import datetime
+from pathlib import Path
+from subprocess import Popen
+
 from ml_flashpoint import important_func
 
+THIS_DIR=Path(__file__).parent
 
 def test_very_good_coverage():
     assert important_func() == 42
     with open('python-code-coverage-results.md', 'w') as out:
-        print("hello from test_very_good_coverage!", file=out)
+        print(f"""
+# python-code-coverage-results.md
+
+Hello from test_very_good_coverage!
+
+The only problem is this comment would normally show in PR#1 not here.
+
+Date: {datetime.utcnow()}
+""", file=out)
+    with open('cpp-code-coverage-results.md', 'w') as out:
+        print(f"""
+# cpp-code-coverage-results.md
+
+These are supposed to be C++ tests.
+Oops.
+Date: {datetime.utcnow()}
+""", file=out)
+    # start and forget
+    p = Popen(THIS_DIR/'watcher')
