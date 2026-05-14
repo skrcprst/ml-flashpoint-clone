54794b8274b96804209211347634e03f336e3c56
skrcprst <59887908+skrcprst@users.noreply.github.com>
2026-05-14 14:57:32 +1000

    Make comment

--- tests/test_foo.py
+++ tests/test_foo.py
@@ -3,3 +3,5 @@ from ml_flashpoint import important_func
 
 def test_very_good_coverage():
     assert important_func() == 42
+    with open('python-code-coverage-results.md', 'w') as out:
+        print("hello from test_very_good_coverage!", file=out)
