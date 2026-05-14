376f69383eb7428b59d0d4397fcbf209ba401d82
skrcprst <59887908+skrcprst@users.noreply.github.com>
2026-05-14 14:51:12 +1000

    Skip cmake subdir build

--- run_coverage.sh
+++ run_coverage.sh
@@ -15,40 +15,26 @@
 
 set -ex
 
+rm -rf .venv
+uv venv .venv
+
+set +x
 # Activate virtual environment
 source .venv/bin/activate
+set -x
 
 # Clean up previous coverage runs
 rm -rf python-coverage.xml cxx-coverage.xml htmlcov build
 
 # Re-install with C++ coverage enabled
-pip install -e '.[dev]' --config-settings=cmake.args="-DENABLE_COVERAGE=ON"
+uv pip install -e '.' --config-settings=cmake.args="-DENABLE_COVERAGE=ON" coverage pytest
 
 # Run Python tests with coverage
 coverage run --source=src/ml_flashpoint --branch -m pytest -v -s
 coverage xml -o python-coverage.xml
+coverage html -d htmlcov
 
 coverage report
 
-
-
-# To run directly in the terminal, can run with these options to just show missing test coverage:
-#  pytest --cov=src/ml_flashpoint --cov-branch --cov-report=term-missing term:skip-covered
-
-# Create C++ coverage report directory
-mkdir -p htmlcov/cpp
-
-# Run gcovr for C++ coverage
-gcovr --root=. --filter=src/ml_flashpoint --exclude=".*/_deps/.*" --gcov-executable=gcov --txt-metric branch --html-details htmlcov/cpp/index.html --xml-pretty -o cxx-coverage.xml --sort uncovered-number --gcov-ignore-parse-errors=negative_hits.warn,suspicious_hits.warn
-
-# Generate Python HTML report
-coverage html -d htmlcov/python
-
-# Generate combined coverage report (TODO: not working)
-#mkdir -p htmlcov/combined
-#gcovr --add-tracefile python-coverage.xml --add-tracefile cxx-coverage.xml --html-details -o htmlcov/combined/index.html --xml-pretty -o combined-coverage.xml
-
 echo "Coverage reports generated successfully."
 echo "Python HTML report: htmlcov/python/index.html"
-echo "C++ HTML report: htmlcov/cpp/index.html"
-#echo "Combined HTML report: htmlcov/combined/index.html"
